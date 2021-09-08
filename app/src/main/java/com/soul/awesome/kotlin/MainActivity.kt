e5e1f2dfa37e90286f7ea908a3bd5ecf6dca3672
Soul <soulstayreal@gmail.com>
2021-09-08 14:36:25 +0800

    Activity - 添加一个Send Action方法

--- app/src/main/java/com/soul/awesome/kotlin/MainActivity.kt
+++ app/src/main/java/com/soul/awesome/kotlin/MainActivity.kt
@@ -4,10 +4,17 @@ package com.soul.awesome.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /*点击页面的Send按钮*/
+    fun sendMessage(view: View) {
+        // 发起Send Action
+        println("点击了Send按钮")
     }
 }
