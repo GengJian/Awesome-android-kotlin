61133d38c831cf13cea54d7801cb89dc699d7689
Soul <soulstayreal@gmail.com>
2021-09-08 15:00:43 +0800

    Activity - 创建第二个页面 DisplayMessageActivity

--- app/src/main/java/com/soul/awesome/kotlin/MainActivity.kt
+++ app/src/main/java/com/soul/awesome/kotlin/MainActivity.kt
@@ -2,9 +2,14 @@
 
 package com.soul.awesome.kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+/*val声明不可变的常量，类似于let*/
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,13 @@ class MainActivity : AppCompatActivity() {
     fun sendMessage(view: View) {
         // 发起Send Action
         println("点击了Send按钮")
+
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        // Intent 是在相互独立的组件（如两个 activity）之间提供运行时绑定功能的对象。Intent 表示应用执行某项操作的意图。
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent) //启动一个由Intent指定的DisplayMessageActivity实例
     }
 }
