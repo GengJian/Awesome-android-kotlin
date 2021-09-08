7c9258ebcba34e134da0d5b1d4a7ae2028bd9179
Soul <soulstayreal@gmail.com>
2021-09-08 15:12:06 +0800

    Intent - 通过上下文意图传值（之前的send方法已经实例化了新页面）

--- app/src/main/java/com/soul/awesome/kotlin/DisplayMessageActivity.kt
+++ app/src/main/java/com/soul/awesome/kotlin/DisplayMessageActivity.kt
@@ -2,10 +2,18 @@ package com.soul.awesome.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        // 从唤起此页面的Intent(上下文)中获取extra message
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        // 获取控件并更新text属性
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
