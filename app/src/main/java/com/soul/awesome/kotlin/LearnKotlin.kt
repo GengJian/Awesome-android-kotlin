5587639eefd446b488696dad38f928b4de86200f
Soul <soulstayreal@gmail.com>
2021-11-13 01:51:56 +0800

    Koltlin - 变量&函数写法，及一句话函数的语法糖

--- app/src/main/java/com/soul/awesome/kotlin/LearnKotlin.kt
+++ app/src/main/java/com/soul/awesome/kotlin/LearnKotlin.kt
@@ -1,8 +1,31 @@
 package com.soul.awesome.kotlin
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     println("Hello Kotlin!")
+
+    // 声明变量（带类型）
+    val a: Int = 99
+    // 简写变量（推导类型）
+    val b = 108
+
+    val value1 = largerNumber(a,b)
+    println("larger number is " + value1)
+
+    val value2 = smallerNumber(a,b)
+    println("smaller number is " + value2)
 }
 
-class LearnKotlin {
+// 函数写法
+fun largerNumber(num1: Int, num2: Int): Int {
+    return max(num1, num2)
 }
+// 函数语法糖
+fun smallerNumber(num1: Int, num2: Int): Int = min(num1, num2)
+
+class LearnKotlin {
+
+}
+
