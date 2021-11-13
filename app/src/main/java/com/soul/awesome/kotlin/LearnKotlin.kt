fd20e176d6d9ce504a3ea63385895658464ec4f0
Soul <soulstayreal@gmail.com>
2021-11-13 14:56:07 +0800

    Kotlin - 集合之List

--- app/src/main/java/com/soul/awesome/kotlin/LearnKotlin.kt
+++ app/src/main/java/com/soul/awesome/kotlin/LearnKotlin.kt
@@ -3,6 +3,7 @@ package com.soul.awesome.kotlin
 import kotlin.math.max
 import kotlin.math.min
 
+//<editor-fold desc="主函数">
 fun main() {
     println("Hello Kotlin!")
 
@@ -21,6 +22,20 @@ fun main() {
 //    println(getScore1("Jim"))
 //    println(getScore2("Tommy"))
 
+//    //测试函数写法
+//    testMethod()
+
+//    //测试数据类
+//    testDataObject()
+//
+//    // 测试单例
+//    Singleton.singletonTest()
+
+    testList()
+
+}
+
+private fun testMethod() {
 //    // 通过调用主构造函数，并设置初始值
 //    val student1 = Student("Jack", 19)
 //    // 调用次构造函数
@@ -30,19 +45,20 @@ fun main() {
 //
 //    doStudy(student1)
 //    doStudy(student2)
-
-    //测试数据类
-    val cellphone1 = CellphoneModel("Samsung", 1299.99)
-    val cellphone2 = CellphoneModel("HUAWEI", 4388.88)
-    val cellphone3 = CellphoneModel("Samsung", 1299.99)
-
-    println(cellphone2)
-    println("cellphone1 equals cellphone3 " + cellphone1.equals(cellphone3))
-
-    // 测试单例
-    Singleton.singletonTest()
-
 }
+
+private fun testDataObject() {
+//    val cellphone1 = CellphoneModel("Samsung", 1299.99)
+//    val cellphone2 = CellphoneModel("HUAWEI", 4388.88)
+//    val cellphone3 = CellphoneModel("Samsung", 1299.99)
+//
+//    println(cellphone2)
+//    println("cellphone1 equals cellphone3 " + cellphone1.equals(cellphone3))
+}
+//</editor-fold>
+
+
+//<editor-fold desc="测试实例函数">
 
 // 函数写法
 fun largerNumber(num1: Int, num2: Int): Int {
@@ -74,4 +90,23 @@ fun doStudy(student: Student) {
     student.readBooks()
     student.doHomeworks()
 }
+
+fun testList() {
+    println("测试不可变数组")
+    // 不可变集合
+    val list = listOf<String>("Apple", "Pear", "Banana", "Orange")
+    for (fruit in list) {
+        println(fruit)
+    }
+    println("测试可变数组")
+    // 可变集合
+    val mutableList = mutableListOf<String>("Apple", "Pear", "Banana", "Orange")
+    mutableList.add("WaterMelon")
+    mutableList.remove("Pear")
+    for (fruit in mutableList) {
+        println(fruit)
+    }
+
+}
+//</editor-fold>
 
