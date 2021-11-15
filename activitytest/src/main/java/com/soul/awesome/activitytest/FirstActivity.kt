4015f9596a06de5509eef465cb05aaa0d0ae091f
Soul <soulstayreal@gmail.com>
2021-11-15 13:22:05 +0800

    Plugin - 引入kotlin-android-extensions之后，可以无需FindViewById即获取控件实例

--- activitytest/src/main/java/com/soul/awesome/activitytest/FirstActivity.kt
+++ activitytest/src/main/java/com/soul/awesome/activitytest/FirstActivity.kt
@@ -1,11 +1,24 @@
 package com.soul.awesome.activitytest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.first_layout.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+
+        // 通过FindById 找到控件实例
+//        val button1:Button = findViewById(R.id.button1)
+//        button1.setOnClickListener {
+//            Toast.makeText(this, "You clicked Button 1 ", Toast.LENGTH_SHORT).show()
+//        }
+
+        // Kotlin直接实例
+        button1.setOnClickListener {
+            Toast.makeText(this, "You clicked Button 1 ", Toast.LENGTH_LONG).show()
+        }
     }
 }
