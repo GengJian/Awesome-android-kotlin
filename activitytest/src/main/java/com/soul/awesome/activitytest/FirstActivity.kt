51530f4b6a386c5abebdfaa8bb5090aca7fd4b5e
Soul <soulstayreal@gmail.com>
2021-11-15 13:37:16 +0800

    Menu - 添加右上角的菜单资源

--- activitytest/src/main/java/com/soul/awesome/activitytest/FirstActivity.kt
+++ activitytest/src/main/java/com/soul/awesome/activitytest/FirstActivity.kt
@@ -1,6 +1,8 @@
 package com.soul.awesome.activitytest
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.first_layout.*
@@ -20,5 +22,20 @@ class FirstActivity : AppCompatActivity() {
         button1.setOnClickListener {
             Toast.makeText(this, "You clicked Button 1 ", Toast.LENGTH_LONG).show()
         }
+    }
+
+    // 重写方法，快捷键Control+O
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add ", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove ", Toast.LENGTH_SHORT)
+                .show()
+        }
+        return true
     }
 }
