43bdae4bf5db3331fc20e78d1894ca204965b700
Soul <soulstayreal@gmail.com>
2021-11-16 14:11:15 +0800

    Layout - 自定义Layout 添加响应事件

--- uilayouttest/src/main/java/com/soul/awesome/uilayouttest/TitleLayout.kt
+++ uilayouttest/src/main/java/com/soul/awesome/uilayouttest/TitleLayout.kt
@@ -4,23 +4,26 @@ import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.common_navigationbar.view.*
 
-open class TitleLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+class TitleLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     init {
-        // 动态加载一个xml不惧文件，构建一个inflater对象；并传入一个父视图；从右往左三步合并一步return
+        // 动态加载一个xml布局文件，构建一个inflater对象；并传入一个父视图；从右往左三步合并一步return
         LayoutInflater.from(context).inflate(R.layout.common_navigationbar, this)
 
-//        //添加返回按钮的点击事件
-//        navigationBackBtn.setOnClickListener {
-//         val activity = context as Activity
-//         activity.finish()
-//        }
-
-//        navigationEditButton.setOnClickListener {
-//            Toast.makeText(context, "You clicked Edit button", Toast.LENGTH_SHORT).show()
-//        }
+        val backBtn = findViewById<Button>(R.id.navBackButton)
+        //添加返回按钮的点击事件
+        backBtn.setOnClickListener {
+            val activity = context as Activity
+            activity.finish()
+        }
+        //添加编辑按钮的点击事件
+        val editBtn = findViewById<Button>(R.id.navEditButton)
+        editBtn.setOnClickListener {
+            Toast.makeText(context, "You clicked Edit button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
