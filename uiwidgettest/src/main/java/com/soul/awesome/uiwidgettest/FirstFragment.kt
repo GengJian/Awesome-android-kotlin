b4069099dc3d2a2e1574d872207da53c6669bc39
Soul <soulstayreal@gmail.com>
2021-11-16 10:46:22 +0800

    ImageView - 添加图片，并展示切换

--- uiwidgettest/src/main/java/com/soul/awesome/uiwidgettest/FirstFragment.kt
+++ uiwidgettest/src/main/java/com/soul/awesome/uiwidgettest/FirstFragment.kt
@@ -1,17 +1,21 @@
 package com.soul.awesome.uiwidgettest
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.soul.awesome.uiwidgettest.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -32,13 +36,36 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // A.通过控件绑定lambada表达式
         binding.button.setOnClickListener {
+            Log.d("FirstFragment", "通过viewbinding响应OnClick方法")
+            val inputText = binding.editText.toString()
+            Toast.makeText(this.activity, inputText, Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        // B.通过接口实现
+        binding.button.setOnClickListener(this)
+        binding.changeImageButton.setOnClickListener(this)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /// 通过接口 统一处理onClick方法
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.button -> {
+                Log.d("FirstFragment", "通过接口响应OnClick方法")
+                val inputText = binding.editText.toString()
+                Toast.makeText(this.activity, inputText, Toast.LENGTH_SHORT).show()
+            }
+            R.id.changeImageButton -> {
+                Log.d("FirstFragment", "通过接口响应OnClick方法")
+                binding.imageView.setImageResource(R.drawable.img_2)
+            }
+        }
     }
 }
