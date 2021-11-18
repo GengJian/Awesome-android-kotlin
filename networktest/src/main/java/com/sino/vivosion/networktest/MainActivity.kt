1742b026cf69165ab2334a1adf0d3e57beea65ee
Soul <soulstayreal@gmail.com>
2021-11-18 10:14:25 +0800

    Connection - 添加网络权限并解析response

--- networktest/src/main/java/com/sino/vivosion/networktest/MainActivity.kt
+++ networktest/src/main/java/com/sino/vivosion/networktest/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.sino.vivosion.networktest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendRquestButton.setOnClickListener {
+            sendRequestWithHttpURLConnect()
+        }
+
+    }
+
+    fun sendRequestWithHttpURLConnect() {
+        // 开启线程发起网络请求
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+
+                val input = connection.inputStream
+                // 解析 获取到的输入流
+                var reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    //打印日志至界面
+    fun showResponse(response: String) {
+        runOnUiThread {
+            // UI 操作放至 UI线程中
+            Log.e("请求返回内容", response)
+            responseText.text = response
+        }
     }
 }
