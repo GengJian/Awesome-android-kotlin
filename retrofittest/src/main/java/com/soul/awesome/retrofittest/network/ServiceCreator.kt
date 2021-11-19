84436a274d72ea18bb0c947abcae21728656d5d0
Soul <soulstayreal@gmail.com>
2021-11-19 14:27:33 +0800

    Fix - 添加日志拦截器

--- retrofittest/src/main/java/com/soul/awesome/retrofittest/network/ServiceCreator.kt
+++ retrofittest/src/main/java/com/soul/awesome/retrofittest/network/ServiceCreator.kt
@@ -1,27 +1,32 @@
 package com.soul.awesome.retrofittest.network
 
+import com.soul.awesome.retrofittest.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-open class ServiceCreator {
-    // Retrofit2中要求baseURL必须以/斜线结束，消除歧义
-    private const val HOST_URL = "http://api-t.sino-beta.com/"
+class ServiceCreator {
+
+    // 静态方法&常量
+    companion object {
+        // Retrofit2中要求baseURL必须以/斜线结束，消除歧义
+        private const val HOST_URL = "http://api-t.sino-beta.com/"
+    }
 
     // 实例化OKHttp
-    val httpClient = OkHttpClient.Builder()
-
-    //添加日志拦截器
-//    if (BuildConfig.DEBUG == true) {
-//        val logging = HttpLoggingInterceptor()
-//        logging.level = HttpLoggingInterceptor.Level.BODY
-//        httpClient.addInterceptor(logging)
-//    }
+    val httpClient = OkHttpClient.Builder().apply {
+        //添加日志拦截器
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            addInterceptor(logging)
+        }
+    }
 
     // 实例化Retrofit对象
     private val retrofit = Retrofit.Builder()
-        .baseUrl(HOST_URL)
+        .baseUrl(Companion.HOST_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(null)
         .build()
@@ -31,4 +36,5 @@ open class ServiceCreator {
 
     // 泛型实化
     inline fun <reified T> create(): T = creat(T::class.java)
+}
 
