839cefc969a2427d39548c965099757f1d645648
Soul <soulstayreal@gmail.com>
2021-11-22 18:06:56 +0800

    RecycleView - 不定尺寸瀑布流

--- recyclerviewtest/src/main/java/com/soul/awesome/recyclerviewtest/Fruit.kt
+++ recyclerviewtest/src/main/java/com/soul/awesome/recyclerviewtest/Fruit.kt
@@ -1,3 +1,14 @@
 package com.soul.awesome.listviewtest
 
-class Fruit(val name:String, val imageId: Int)
+class Fruit(val name:String, val imageId: Int) {
+
+    companion object fun getRandomLengthString(str: String): String {
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n) {
+            builder.append(str)
+        }
+        return builder.toString()
+    }
+
+}
--- recyclerviewtest/src/main/java/com/soul/awesome/recyclerviewtest/MainActivity.kt
+++ recyclerviewtest/src/main/java/com/soul/awesome/recyclerviewtest/MainActivity.kt
@@ -26,17 +26,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFruits() {
         repeat(3) {
-            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
-            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
-            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
-            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
-            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
-            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
-            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
-            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
-            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
-            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+            fruitList.add(Fruit(getRandomLengthString("Apple"), R.drawable.apple_pic))
+            fruitList.add(Fruit(getRandomLengthString("Banana"), R.drawable.banana_pic))
+            fruitList.add(Fruit(getRandomLengthString("Orange"), R.drawable.orange_pic))
+            fruitList.add(Fruit(getRandomLengthString("Watermelon"), R.drawable.watermelon_pic))
+            fruitList.add(Fruit(getRandomLengthString("Pear"), R.drawable.pear_pic))
+            fruitList.add(Fruit(getRandomLengthString("Grape"), R.drawable.grape_pic))
+            fruitList.add(Fruit(getRandomLengthString("Pineapple"), R.drawable.pineapple_pic))
+            fruitList.add(Fruit(getRandomLengthString("Strawberry"), R.drawable.strawberry_pic))
+            fruitList.add(Fruit(getRandomLengthString("Cherry"), R.drawable.cherry_pic))
+            fruitList.add(Fruit(getRandomLengthString("Mango"), R.drawable.mango_pic))
         }
     }
 
+    fun getRandomLengthString(str: String): String {
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n) {
+            builder.append(str)
+        }
+        return builder.toString()
+    }
 }
