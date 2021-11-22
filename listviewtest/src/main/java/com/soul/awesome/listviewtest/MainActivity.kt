4ea8d31da391baa58c0a89b8ae49f5c57d75305c
Soul <soulstayreal@gmail.com>
2021-11-22 15:54:28 +0800

    Fix - listview adapter

--- listviewtest/src/main/java/com/soul/awesome/listviewtest/MainActivity.kt
+++ listviewtest/src/main/java/com/soul/awesome/listviewtest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.soul.awesome.listviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private val data = listOf<String>("Apple", "Banana","Orange","Cherry")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        val listView = findViewById<ListView>(R.id.test_listview)
+        listView.adapter = adapter
     }
 }
